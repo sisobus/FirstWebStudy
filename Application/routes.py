1311b8a832d63a5012ac281087602f47ac21ecb4
sangkeun kim <sisobus@vuno.co>
2017-02-08 21:07:54 +0900

    첫날은 여기까지

--- Application/routes.py
+++ Application/routes.py
@@ -4,5 +4,13 @@ from flask import Flask, url_for
 from flask import render_template, flash, redirect, session, request, g, jsonify
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/jisung')
+def jisung():
+    return render_template('jisung.html')
+
 if __name__ == '__main__':
-    app.run(host='14.63.88.216',port=5003,debug=True,threaded=True)
+    app.run(host='14.63.88.216',port=5999,debug=True,threaded=True)
